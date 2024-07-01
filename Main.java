49d8478a2238f95ddbeb458325f32ad0040f9a94
descifrador93 <nateriverace1@gmail.com>
2024-07-01 17:35:17 +0530

    Updating Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,30 @@
 class Main {
+
+    public boolean isSafe (char [][] board, int row, int col, int number) {
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][col] == (char) (number + '0')) {
+                return false;
+            }
+
+            if (board[row][i] == (char) (number + '0')) {
+                return false;
+            }
+        }
+
+        int sr = row/3 * 3;
+        int sc = col/3 * 3;
+
+        for (int i = sr; i < sr + 3; i++) {
+            for (int j = sc; j < sc +3; j++) {
+                if (board[i][j] == (char) (number + '0')) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+
     public boolean helper (char [][] board, int row, int col) {
 
         if (row == board.length) {
